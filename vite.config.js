e1257c82a8e91c65b8d5f7e1e4733b2ed0428652
Gautham G Nayak <80469943+gautham-g-nayak@users.noreply.github.com>
2024-06-28 14:39:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/anime-info/",
+  base: "https://gautham-g-nayak.github.io/anime-info/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
